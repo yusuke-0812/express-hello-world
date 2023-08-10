855ddabfbb22e3223b11467360789aadedc0794a
Yusuke Mori <yusukemori@YusukenoMacBook-Pro.local>
2023-08-10 20:00:14 +0900

    htmlを変更

--- app.js
+++ app.js
@@ -11,51 +11,54 @@ server.headersTimeout = 120 * 1000;
 
 const html = `
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>簡単なチャットアプリ</title>
     <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
+        #chatArea {
+            border: 1px solid black;
+            height: 300px;
+            overflow-y: scroll;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
+
+        #chatArea p {
+            margin: 5px 0;
+        }
     </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
+</head>
+
+<body>
+
+<div id="chatArea">
+</div>
+
+<input type="text" id="chatInput" placeholder="メッセージを入力...">
+<button onclick="sendMessage()">送信</button>
+
+<script>
+    function sendMessage() {
+        const chatInput = document.getElementById('chatInput');
+        const chatArea = document.getElementById('chatArea');
+
+        const message = chatInput.value;
+
+        if (message.trim() === "") return;  // 何も入力されていない場合は送信しない
+
+        const messageElement = document.createElement('p');
+        messageElement.textContent = message;
+
+        chatArea.appendChild(messageElement);
+
+        chatInput.value = "";  // 入力エリアをクリア
+        chatArea.scrollTop = chatArea.scrollHeight;  // スクロールを最下部に移動
+    }
+</script>
+
+</body>
 </html>
 `
