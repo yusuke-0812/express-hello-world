00809d5d75424e27b4ddf5f0dc07999d2a08c9cf
Yusuke Mori <yusukemori@YusukenoMacBook-Pro.local>
2023-08-24 19:46:06 +0900

    ユーザー登録ページ追加

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
+app.get("/", (req, res) => res.type('html').send(html));
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const { Pool } = require("pg");
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// PostgreSQLの設定
+const pool = new Pool({
+    connectionString: "PGPASSWORD=VHUSXvYdJT7rvwwV75seWM3cMy2s1hWv psql -h dpg-cjad1llm2m9c73c8ese0-a.oregon-postgres.render.com -U chatuserdb_user chatuserdb",
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.type("html").send(html));
+
+app.get("/signup", (req, res) => {
+    // ここでユーザー登録用のHTMLを提供します
+    res.sendFile(path.join(__dirname, "./html", "register.html"));
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).send("Missing parameters");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", [username, email, hashedPassword]);
+
+        res.status(200).send("User registered successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
+const server = app.listen(port, () => console.log(`Chat app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
+const html =  `
 <!DOCTYPE html>
 <html lang="ja">
 
